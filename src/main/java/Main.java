4aeb3dc3b099decbae416ee1a68cebea398ae58d
Kamil Skubis <kskubis@gmail.com>
2022-06-21 14:37:29 +0200

    for testing use one binanceBar, no need to fetch data from binance

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,6 +6,7 @@ import org.hibernate.Transaction;
 import persistence.HibernateUtil;
 
 import java.util.LinkedHashMap;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Main {
@@ -14,11 +15,22 @@ public class Main {
         SpotClientImpl client = new SpotClientImpl();
         BinanceDownloader binance = new BinanceDownloader(client.createMarket());
 
-        LinkedHashMap<String, Object> params = new LinkedHashMap<>();
-        params.put("symbol", "BTCUSDT");
-        params.put("interval", "1d");
-        params.put("limit", "20");
-        List<BinanceBar> datas = binance.downloadKlines(params);
+//        LinkedHashMap<String, Object> params = new LinkedHashMap<>();
+//        params.put("symbol", "BTCUSDT");
+//        params.put("interval", "1d");
+//        params.put("limit", "20");
+//        List<BinanceBar> datas = binance.downloadKlines(params);
+
+        List<BinanceBar> datas = new LinkedList<>();
+        BinanceBar b = new BinanceBar();
+        b.setTicker("test");
+        b.setOpen(2313.03);
+        b.setHigh(2323.00);
+        b.setLow(2300.31);
+        b.setClose(2312.23);
+        b.setVolume(1000.00);
+        b.setOpenTime(1231232l);
+        datas.add(b);
 
         Session s = HibernateUtil.getSessionFactory().openSession();
         Transaction t = s.beginTransaction();
