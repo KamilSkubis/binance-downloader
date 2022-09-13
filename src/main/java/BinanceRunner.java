843bbaef41b9dbfee22c4258e538deb9ba06e884
Baanao <kskubis@gmail.com>
2022-09-13 21:56:41 +0200

    save

--- src/main/java/BinanceRunner.java
+++ src/main/java/BinanceRunner.java
@@ -108,7 +108,7 @@ public class BinanceRunner {
                 System.out.println("symbol " + symbol + " , found in database");
 
                 final LocalDateTime dateInDb = symbolTimeFromDb.get(symbol);
-                final LocalDateTime newStartDate = dateInDb.plusSeconds(1); // TODO change this when downloading daily, or refactor
+                final LocalDateTime newStartDate = dateInDb.plusMinutes(1); // TODO change this when downloading daily, or refactor
 
                 System.out.println("found latest date: " + dateInDb + " new calculated date: " + newStartDate);
                 Instant inst = newStartDate.toInstant(ZoneOffset.UTC);
