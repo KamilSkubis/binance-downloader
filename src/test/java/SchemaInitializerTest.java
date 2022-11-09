b2fc74505c5b0e819b85dd81cdf452804ec0df29
kskubis@gmail.com <kskubis@gmail.com>
2022-11-09 21:10:24 +0100

    clean up in SchemaInitializerTest

--- src/test/java/SchemaInitializerTest.java
+++ src/test/java/SchemaInitializerTest.java
@@ -1,40 +1,33 @@
 import org.hibernate.Session;
-import org.hibernate.query.NativeQuery;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.mockito.stubbing.OngoingStubbing;
 import persistence.MySQLUtilTesting;
 import persistence.SchemaInitializer;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import static org.mockito.Mockito.*;
 
 public class SchemaInitializerTest {
 
     @Before
-    public void before(){
+    public void before() {
         UtilForTesting.dropTables();
     }
 
     @Test
-    public void initTables_whenNoTables_IntegrationTest(){
-        Session session =  MySQLUtilTesting.getSessionFactory().openSession();
+    public void initTables_whenNoTables_IntegrationTest() {
+        Session session = MySQLUtilTesting.getSessionFactory().openSession();
         SchemaInitializer schemaInitializer = new SchemaInitializer(session);
         schemaInitializer.initializeSchemasOrDoNothing();
 
         var s = MySQLUtilTesting.getSessionFactory().openSession();
 
         int size = s.createSQLQuery("show tables").getResultList().size();
-        Assertions.assertEquals(2,size);
+        Assertions.assertEquals(2, size);
     }
 
 
     @After
-    public void after(){
+    public void after() {
         UtilForTesting.dropTables();
     }
 
